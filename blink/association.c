852a54f699085d3aa14e8a0d46c91efdd1c97857
Geovane Fedrecheski <geonnave@gmail.com>
2025-02-27 15:40:01 +0100

    assoc: add new association module

---
+++ blink/association.c
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * @ingroup     blink_assoc
+ *
+ * @brief       Association procedure for the blink protocol
+ *
+ * @author Geovane Fedrecheski <geovane.fedrecheski@inria.fr>
+ *
+ * @copyright Inria, 2024
+ */
+
+#include <nrf.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "blink.h"
+#include "mac.h"
+#include "scan.h"
+#include "scheduler.h"
+
+//=========================== defines =========================================
+
+typedef enum {
+    JOIN_STATE_IDLE = 1,
+    JOIN_STATE_SCANNING = 2,
+    JOIN_STATE_SYNCED = 4,
+    JOIN_STATE_JOINING = 8,
+    JOIN_STATE_JOINED = 16,
+} bl_assoc_state_t;
+
+typedef struct {
+    // bl_node_type_t          node_type; // get from blink: bl_get_node_type()
+    bl_assoc_state_t state;
+} assoc_vars_t;
+
+//=========================== variables =======================================
+
+static assoc_vars_t assoc_vars = { 0 };
+
+//=========================== prototypes ======================================
+
+
+//=========================== public ==========================================
+
+void bl_assoc_init(void) {
+    assoc_vars.state = JOIN_STATE_IDLE;
+}
+
+//=========================== callbacks =======================================
+
+//=========================== private =========================================
---
+++ blink/association.h
@@ -0,0 +1,27 @@
+#ifndef __ASSOCIATION_H
+#define __ASSOCIATION_H
+
+/**
+ * @file
+ * @ingroup     blink_assoc
+ *
+ * @brief       Association procedure for the blink protocol
+ *
+ * @author Geovane Fedrecheski <geovane.fedrecheski@inria.fr>
+ *
+ * @copyright Inria, 2024-now
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "protocol.h"
+
+//=========================== defines ==========================================
+
+//=========================== variables ========================================
+
+//=========================== prototypes =======================================
+
+void bl_assoc_init(void);
+
+#endif // __ASSOCIATION_H
