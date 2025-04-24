5615396d5469430660547dfd5b98bc08f96d3516
Geovane Fedrecheski <geonnave@gmail.com>
2025-04-24 11:31:26 +0200

    app/03app_node: show connection status on minimote rgb leg

--- app/03app_node/main.c
+++ app/03app_node/main.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include "bl_gpio.h"
 #include "bl_radio.h"
 #include "bl_timer_hf.h"
 #include "blink.h"
@@ -25,7 +26,18 @@ typedef struct {
     bool dummy;
 } node_vars_t;
 
+typedef enum {
+    OFF,
+    RED,
+    GREEN,
+    BLUE,
+} led_color_t;
+
 //=========================== variables ========================================
+
+bl_gpio_t _r_led_pin = { .port = 0, .pin = 28 };
+bl_gpio_t _g_led_pin = { .port = 0, .pin = 2 };
+bl_gpio_t _b_led_pin = { .port = 0, .pin = 3 };
 
 node_vars_t node_vars = { 0 };
 
@@ -36,6 +48,8 @@ extern schedule_t schedule_minuscule, schedule_tiny, schedule_small, schedule_hu
 
 //=========================== prototypes =======================================
 
+static void debug_init_minimote(void);
+static void debug_set_rgb(led_color_t color);
 static void blink_event_callback(bl_event_t event, bl_event_data_t event_data);
 
 //=========================== main =============================================
@@ -44,6 +58,8 @@ int main(void)
 {
     printf("Hello Blink Node\n");
     bl_timer_hf_init(BLINK_APP_TIMER_DEV);
+    debug_init_minimote();
+    debug_set_rgb(RED);
 
     blink_init(BLINK_NODE, &schedule_minuscule, &blink_event_callback);
 
@@ -72,22 +88,93 @@ static void blink_event_callback(bl_event_t event, bl_event_data_t event_data) {
                 printf("%02X ", packet.payload[i]);
             }
             printf("\n");
+            debug_set_rgb(BLUE);
+            bl_timer_hf_delay_ms(BLINK_APP_TIMER_DEV, 50);
+            debug_set_rgb(GREEN);
             break;
         }
         case BLINK_CONNECTED: {
             uint64_t gateway_id = event_data.data.gateway_info.gateway_id;
             printf("Connected to gateway %016llX\n", gateway_id);
+            debug_set_rgb(GREEN);
             break;
         }
         case BLINK_DISCONNECTED: {
             uint64_t gateway_id = event_data.data.gateway_info.gateway_id;
             printf("Disconnected from gateway %016llX, reason: %u\n", gateway_id, event_data.tag);
+            debug_set_rgb(RED);
             break;
         }
         case BLINK_ERROR:
             printf("Error\n");
             break;
         default:
+            break;
+    }
+}
+
+//=========================== debug ========================================
+
+static void debug_init_minimote(void) {
+#ifndef BOARD_NRF52833DK
+    return;
+#endif
+    // Make sure the mini-mote is running at 3.0v
+    // Might need a re-start to take effect
+    if (NRF_UICR->REGOUT0 != UICR_REGOUT0_VOUT_3V0) {
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+        }
+        NRF_UICR->REGOUT0 = UICR_REGOUT0_VOUT_3V0;
+
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+        }
+    }
+    bl_gpio_init(&_r_led_pin, BL_GPIO_OUT);
+    bl_gpio_init(&_g_led_pin, BL_GPIO_OUT);
+    bl_gpio_init(&_b_led_pin, BL_GPIO_OUT);
+
+    bl_gpio_t _reg_pin = { .port = 0, .pin = 30 };
+    // Turn ON the DotBot board regulator if provided
+    bl_gpio_init(&_reg_pin, BL_GPIO_OUT);
+    bl_gpio_set(&_reg_pin);
+}
+
+
+static void debug_set_rgb(led_color_t color) {
+#ifndef BOARD_NRF52833DK
+    return;
+#endif
+    switch (color) {
+        case RED:
+            bl_gpio_clear(&_r_led_pin);
+            bl_gpio_set(&_g_led_pin);
+            bl_gpio_set(&_b_led_pin);
+            break;
+
+        case GREEN:
+            bl_gpio_set(&_r_led_pin);
+            bl_gpio_clear(&_g_led_pin);
+            bl_gpio_set(&_b_led_pin);
+            break;
+
+        case BLUE:
+            bl_gpio_set(&_r_led_pin);
+            bl_gpio_set(&_g_led_pin);
+            bl_gpio_clear(&_b_led_pin);
+            break;
+
+        case OFF:
+            bl_gpio_set(&_r_led_pin);
+            bl_gpio_set(&_g_led_pin);
+            bl_gpio_set(&_b_led_pin);
+            break;
+
+        default:
+            bl_gpio_set(&_r_led_pin);
+            bl_gpio_set(&_g_led_pin);
+            bl_gpio_set(&_b_led_pin);
             break;
     }
 }
