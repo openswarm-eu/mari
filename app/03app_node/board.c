ad2ebb8adcf0a93a3d0faf6b69eff49f96066ff8
offmor <153065494+offmor@users.noreply.github.com>
2025-06-27 21:28:24 +0800

    feat: Implement board definition

--- app/03app_node/board.c
+++ app/03app_node/board.c
@@ -4,18 +4,25 @@
  * @details     
  *              
  */
+
 #include "board.h"
 #include "board_config.h"
 #include "mr_gpio.h"
 #include <nrf.h>
 
-// --- MINIMOTE IMPLEMENTATION ---
+// --- PIN DEFINITIONS ---
 
 #ifdef MINIMOTE
+static mr_gpio_t _r_led_pin = {.port = 0, .pin = 28};
+static mr_gpio_t _g_led_pin = {.port = 0, .pin = 2};
+static mr_gpio_t _b_led_pin = {.port = 0, .pin = 3};
+#endif
 
-static mr_gpio_t _r_led_pin = {.port = 0,.pin = 28 };
-static mr_gpio_t _g_led_pin = {.port = 0,.pin = 2 };
-static mr_gpio_t _b_led_pin = {.port = 0,.pin = 3 };
+#ifdef NRF52840_DONGLE
+static mr_gpio_t _r_led_pin = {.port = 0, .pin = 8};   // LED 2 Red
+static mr_gpio_t _g_led_pin = {.port = 1, .pin = 9};   // LED 2 Green
+static mr_gpio_t _b_led_pin = {.port = 0, .pin = 12};  // LED 2 Blue
+#endif
 
 void board_init(void) {
 #ifdef BOARD_NRF52833DK
@@ -69,57 +76,3 @@ void board_set_rgb(led_color_t color) {
             break;
     }
 }
-
-#endif 
-
-// --- NRF52840 DONGLE(PCA10059) IMPLEMENTATION ---
-
-#ifdef NRF52840_DONGLE
-
-static mr_gpio_t _r_led_pin = {.port = 0,.pin = 8 };  // LED 2 Red
-static mr_gpio_t _g_led_pin = {.port = 1,.pin = 9 };  // LED 2 Green
-static mr_gpio_t _b_led_pin = {.port = 0,.pin = 12 };  // LED 2 Blue
-
-void board_init(void) {
-    mr_gpio_init(&_r_led_pin, MR_GPIO_OUT);
-    mr_gpio_init(&_g_led_pin, MR_GPIO_OUT);
-    mr_gpio_init(&_b_led_pin, MR_GPIO_OUT);
-    
-    board_set_rgb(BLUE);
-}
-
-void board_set_rgb(led_color_t color) {
-
-    switch (color) {
-        case RED:
-            mr_gpio_clear(&_r_led_pin);
-            mr_gpio_set(&_g_led_pin);
-            mr_gpio_set(&_b_led_pin);
-            break;
-        case GREEN:
-            mr_gpio_set(&_r_led_pin);
-            mr_gpio_clear(&_g_led_pin);
-            mr_gpio_set(&_b_led_pin);
-            break;
-        case BLUE:
-            mr_gpio_set(&_r_led_pin);
-            mr_gpio_set(&_g_led_pin);
-            mr_gpio_clear(&_b_led_pin);
-            break;
-        case OTHER: 
-            mr_gpio_clear(&_r_led_pin);
-            mr_gpio_clear(&_g_led_pin);
-            mr_gpio_clear(&_b_led_pin);
-            break;
-        case OFF:
-        default:
-            mr_gpio_set(&_r_led_pin);
-            mr_gpio_set(&_g_led_pin);
-            mr_gpio_set(&_b_led_pin);
-            break;
-    }
-}
-
-#endif 
-
-
