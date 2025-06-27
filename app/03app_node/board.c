4b709c335e14f96fc802cfe86f65b5c990dc8e7a
offmor <153065494+offmor@users.noreply.github.com>
2025-06-27 22:01:27 +0800

    Apply clang-format fixes

--- app/03app_node/board.c
+++ app/03app_node/board.c
@@ -1,8 +1,8 @@
 /**
  * @file
  * @brief       Board-specific implementations.
- * @details     
- *              
+ * @details
+ *
  */
 
 #include "board.h"
@@ -13,25 +13,25 @@
 // --- PIN DEFINITIONS ---
 
 #ifdef MINIMOTE
-static mr_gpio_t _r_led_pin = {.port = 0, .pin = 28};
-static mr_gpio_t _g_led_pin = {.port = 0, .pin = 2};
-static mr_gpio_t _b_led_pin = {.port = 0, .pin = 3};
+static mr_gpio_t _r_led_pin = { .port = 0, .pin = 28 };
+static mr_gpio_t _g_led_pin = { .port = 0, .pin = 2 };
+static mr_gpio_t _b_led_pin = { .port = 0, .pin = 3 };
 #endif
 
 #ifdef NRF52840_DONGLE
-static mr_gpio_t _r_led_pin = {.port = 0, .pin = 8};   // LED 2 Red
-static mr_gpio_t _g_led_pin = {.port = 1, .pin = 9};   // LED 2 Green
-static mr_gpio_t _b_led_pin = {.port = 0, .pin = 12};  // LED 2 Blue
+static mr_gpio_t _r_led_pin = { .port = 0, .pin = 8 };   // LED 2 Red
+static mr_gpio_t _g_led_pin = { .port = 1, .pin = 9 };   // LED 2 Green
+static mr_gpio_t _b_led_pin = { .port = 0, .pin = 12 };  // LED 2 Blue
 #endif
 
 void board_init(void) {
 #ifdef BOARD_NRF52833DK
     // Make sure the mini-mote is running at 3.0v
-    if (NRF_UICR->REGOUT0!= UICR_REGOUT0_VOUT_3V0) {
+    if (NRF_UICR->REGOUT0 != UICR_REGOUT0_VOUT_3V0) {
         NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
         while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {}
         NRF_UICR->REGOUT0 = UICR_REGOUT0_VOUT_3V0;
-        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+        NRF_NVMC->CONFIG  = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
         while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {}
     }
 #endif
@@ -40,7 +40,7 @@ void board_init(void) {
     mr_gpio_init(&_b_led_pin, MR_GPIO_OUT);
     board_set_rgb(BLUE);
 
-    mr_gpio_t _reg_pin = {.port = 0,.pin = 30 };
+    mr_gpio_t _reg_pin = { .port = 0, .pin = 30 };
     // Turn ON the DotBot board regulator if provided
     mr_gpio_init(&_reg_pin, MR_GPIO_OUT);
     mr_gpio_set(&_reg_pin);
