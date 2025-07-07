2be9ef530cfded6dc91b8c2023de29c8cddca7ac
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-07-07 15:14:16 +0200

    app/03app_gateway_app: implement application side of mira gateway

--- app/03app_gateway_app/main.c
+++ app/03app_gateway_app/main.c
@@ -12,47 +12,33 @@
 #include <nrf.h>
 #include <stdio.h>
 
+#include "ipc.h"
+
 #include "mr_device.h"
-#include "mr_gpio.h"
-#include "mira.h"
+#include "hdlc.h"
+#include "uart.h"
 
 //=========================== defines ==========================================
 
+#define MR_UART_INDEX (1)  ///< Index of UART peripheral to use
+#define MR_UART_BAUDRATE    (1000000UL)  ///< UART baudrate used by the gateway
+
 typedef struct {
-    bool dummy;
+    bool mira_frame_received;
+    bool uart_byte_received;
+    uint8_t uart_byte;
+    uint8_t hdlc_encode_buffer[1024];  // Should be large enough
 } gateway_app_vars_t;
 
-//=========================== variables ========================================
+// UART RX and TX pins
+static const mr_gpio_t _mr_uart_tx_pin = { .port = 1, .pin = 1 };
+static const mr_gpio_t _mr_uart_rx_pin = { .port = 1, .pin = 0 };
 
-gateway_app_vars_t gateway_app_vars = { 0 };
+static gateway_app_vars_t _app_vars = { 0 };
+volatile __attribute__((section(".shared_data"))) ipc_shared_data_t ipc_shared_data;
 
-//=========================== prototypes =======================================
 
-void _setup_debug_pins(void);
-
-//=========================== main =============================================
-
-int main(void) {
-    printf("Hello Mira Gateway App Core (UART) %016llX\n", mr_device_id());
-
-    _setup_debug_pins();
-
-    // TODO: communicate with the network core via IPC, and make sure we start the network core
-
-    // TODO: communicate with an external device via UART (e.g. a computer or raspberry pi)
-
-    while (1) {
-        __SEV();
-        __WFE();
-        __WFE();
-    }
-}
-
-//=========================== callbacks ========================================
-
-//=========================== private ========================================
-
-void _setup_debug_pins(void) {
+static void _setup_debug_pins(void) {
     // Assign P0.28 to P0.31 to the network core (for debugging association.c via LEDs)
     NRF_P0_S->PIN_CNF[28] = GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
     NRF_P0_S->PIN_CNF[29] = GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
@@ -68,4 +54,91 @@ void _setup_debug_pins(void) {
     // Configure all GPIOs as non secure
     NRF_SPU_S->GPIOPORT[0].PERM = 0;
     NRF_SPU_S->GPIOPORT[1].PERM = 0;
+}
+
+static void _configure_ram_non_secure(uint8_t start_region, size_t length) {
+    for (uint8_t region = start_region; region < start_region + length; region++) {
+        NRF_SPU_S->RAMREGION[region].PERM = (SPU_RAMREGION_PERM_READ_Enable << SPU_RAMREGION_PERM_READ_Pos |
+                                             SPU_RAMREGION_PERM_WRITE_Enable << SPU_RAMREGION_PERM_WRITE_Pos |
+                                             SPU_RAMREGION_PERM_EXECUTE_Enable << SPU_RAMREGION_PERM_EXECUTE_Pos |
+                                             SPU_RAMREGION_PERM_SECATTR_Non_Secure << SPU_RAMREGION_PERM_SECATTR_Pos);
+    }
+}
+
+static void _init_ipc(void) {
+    NRF_IPC_NS->INTENSET                            = (1 << IPC_CHAN_RADIO_TO_UART);
+    NRF_IPC_NS->SEND_CNF[IPC_CHAN_UART_TO_RADIO]    = (1 << IPC_CHAN_UART_TO_RADIO);
+    NRF_IPC_NS->RECEIVE_CNF[IPC_CHAN_RADIO_TO_UART] = (1 << IPC_CHAN_RADIO_TO_UART);
+
+    NVIC_EnableIRQ(IPC_IRQn);
+    NVIC_ClearPendingIRQ(IPC_IRQn);
+    NVIC_SetPriority(IPC_IRQn, IPC_IRQ_PRIORITY);
+}
+
+static void _release_network_core(void) {
+    // Do nothing if network core is already started and ready
+    if (!NRF_RESET_S->NETWORK.FORCEOFF && ipc_shared_data.net_ready) {
+        return;
+    } else if (!NRF_RESET_S->NETWORK.FORCEOFF) {
+        ipc_shared_data.net_ready = false;
+    }
+
+    NRF_RESET_S->NETWORK.FORCEOFF = (RESET_NETWORK_FORCEOFF_FORCEOFF_Release << RESET_NETWORK_FORCEOFF_FORCEOFF_Pos);
+
+    while (!ipc_shared_data.net_ready) {}
+}
+
+static void _uart_callback(uint8_t byte) {
+    _app_vars.uart_byte = byte;
+    _app_vars.uart_byte_received = true;
+}
+
+int main(void) {
+    printf("Hello Mira Gateway App Core (UART) %016llX\n", mr_device_id());
+
+    _setup_debug_pins();
+    
+    _configure_ram_non_secure(2, 1);
+    _init_ipc();
+    _release_network_core();
+
+    mr_uart_init(MR_UART_INDEX, &_mr_uart_rx_pin, &_mr_uart_tx_pin, MR_UART_BAUDRATE, &_uart_callback);
+
+    while (1) {
+        __WFE();
+
+        if (_app_vars.uart_byte_received) {
+            _app_vars.uart_byte_received = false;
+            db_hdlc_state_t hdlc_state = db_hdlc_rx_byte(_app_vars.uart_byte);
+            switch ((uint8_t)hdlc_state) {
+                case DB_HDLC_STATE_IDLE:
+                case DB_HDLC_STATE_RECEIVING:
+                case DB_HDLC_STATE_ERROR:
+                    break;
+                case DB_HDLC_STATE_READY:
+                {
+                    size_t msg_len = db_hdlc_decode((uint8_t *)ipc_shared_data.uart_to_radio);
+                    ipc_shared_data.uart_to_radio_len = msg_len;
+                    if (msg_len) {
+                        NRF_IPC_NS->TASKS_SEND[IPC_CHAN_UART_TO_RADIO] = 1;
+                    }
+                } break;
+                default:
+                    break;
+            }
+        }
+
+        if (_app_vars.mira_frame_received) {
+            _app_vars.mira_frame_received = false;
+            size_t frame_len = db_hdlc_encode((uint8_t *)ipc_shared_data.radio_to_uart, ipc_shared_data.radio_to_uart_len, _app_vars.hdlc_encode_buffer);
+            mr_uart_write(MR_UART_INDEX, _app_vars.hdlc_encode_buffer, frame_len);
+        }
+    }
+}
+
+void IPC_IRQHandler(void) {
+    if (NRF_IPC_NS->EVENTS_RECEIVE[IPC_CHAN_UART_TO_RADIO]) {
+        NRF_IPC_NS->EVENTS_RECEIVE[IPC_CHAN_UART_TO_RADIO] = 0;
+        _app_vars.mira_frame_received = true;
+    }
 }
